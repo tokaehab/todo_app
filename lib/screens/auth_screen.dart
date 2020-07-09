f6dc1b329c3192fafc21d456f29caa1b9747ed4a
Toka Ehab <toka.ehabb@gmail.com>
2020-07-09 02:02:47 +0200

    Add firebase and finish Authentication section

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,146 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import '../widgets/auth_form.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  bool isLogin = true;
+  bool isLoading = false;
+  final _auth = FirebaseAuth.instance;
+  void submitFn(
+      String email, String username, String password, BuildContext ctx) async {
+    AuthResult authResult;
+    try {
+      setState(() {
+        isLoading = true;
+      });
+      if (isLogin) {
+        authResult = await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        authResult = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+        await Firestore.instance
+            .collection('users')
+            .document(authResult.user.uid)
+            .setData({
+          'username': username,
+          'email': email,
+        });
+      }
+    } on PlatformException catch (error) {
+      var message = 'An error occurred, Please check your credentials';
+      if (error.message != null) {
+        message = error.message;
+      }
+      Scaffold.of(ctx).showSnackBar(
+        SnackBar(
+          content: Text(message),
+          backgroundColor: Theme.of(ctx).errorColor,
+        ),
+      );
+      setState(() {
+        isLoading = false;
+      });
+    } catch (error) {
+      print(error);
+      Scaffold.of(ctx).showSnackBar(
+        SnackBar(
+          content: Text('An error occurred, Please try again.'),
+          backgroundColor: Theme.of(ctx).errorColor,
+        ),
+      );
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SingleChildScrollView(
+        child: Column(children: <Widget>[
+          SizedBox(
+            height: 100,
+          ),
+          Center(
+            child: Text(
+              'Todo List',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                fontFamily: Theme.of(context).textTheme.title.fontFamily,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.05,
+            ),
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.4,
+            child: Card(
+              elevation: 10,
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: AuthForm(submitFn, isLoading, isLogin),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          Row(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(left: 8.0),
+                    child: Text(
+                      isLogin
+                          ? 'Create new account'
+                          : 'I already have an account?',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        isLogin = !isLogin;
+                      });
+                    },
+                    child: Text(
+                      !isLogin ? 'Login Now' : 'Register Now',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/auth_form.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  final Function submitFn;
+  final bool isLoading;
+  final bool isLogin;
+  AuthForm(this.submitFn, this.isLoading, this.isLogin);
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = '';
+  String _password = '';
+  String _username = '';
+
+  void _trySubmit() {
+    final _isValid = _formKey.currentState.validate();
+    FocusScope.of(context).unfocus();
+    if (_isValid) {
+      _formKey.currentState.save();
+      widget.submitFn(
+        _email.trim(),
+        _username.trim(),
+        _password.trim(),
+        context,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextFormField(
+                key: ValueKey('email'),
+                decoration: InputDecoration(
+                  labelText: 'email',
+                ),
+                validator: (value) {
+                  if (!value.contains('@') ||
+                      !value.contains('.com') ||
+                      value.isEmpty) return 'Invalid Email';
+                  return null;
+                },
+                onSaved: (value) {
+                  _email = value;
+                },
+              ),
+              if (!widget.isLogin)
+                TextFormField(
+                  key: ValueKey('username'),
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                  ),
+                  validator: (value) {
+                    if (value.length < 4)
+                      return 'Username should be at least 4 characters';
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _username = value;
+                  },
+                ),
+              TextFormField(
+                key: ValueKey('password'),
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                ),
+                obscureText: true,
+                validator: (value) {
+                  if (value.length < 8)
+                    return 'Password should be at least 8 characters';
+                  return null;
+                },
+                onSaved: (value) {
+                  _password = value;
+                },
+              ),
+              widget.isLoading
+                  ? Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    )
+                  : RaisedButton(
+                      onPressed: _trySubmit,
+                      child: Text(
+                        widget.isLogin ? 'Log in' : 'Sign up',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      color: Theme.of(context).primaryColor,
+                    ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
